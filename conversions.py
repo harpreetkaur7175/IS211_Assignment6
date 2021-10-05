0d5d57117b768ed5049620b13fe8bb78ee385106
harpreetkaur7175 <89755899+harpreetkaur7175@users.noreply.github.com>
2021-10-04 21:49:48 -0400

    Update conversions.py

--- conversions.py
+++ conversions.py
@@ -1,13 +1,46 @@
 
 def convertCelsiusToKelvin(celsius):
-    """Takes in a float representing a Celsius measurement, and returns that temperature converted into Kelvins"
-    kelvins = 0
-    
-    return kelvins
+	kelvin = 273.15 + celsius
+	print("This is the temperature in degrees Kelvin: ", kelvin)
+
+	return kelvin
 
 
 def convertCelsiusToFahrenheit(celsius):
-    """Takes in a float representing a Celsius measurement, and returns that temperature converted into Fahrenheit"""
-    fahrenheit = 0
-    
-    return fahrenheit
+	fahrenheit = 1.8 * celsius + 32
+	print("This is the temperature in degrees Fahrenheit: ", fahrenheit)
+
+	return fahrenheit
+
+def convertFahrenheitToCelsius(fahrenheit):
+
+	celsius = ((fahrenheit - 32) * (5/9))
+	print("This is the temperature in degrees Celsius: ", celsius)
+
+	return celsius
+
+def convertFahrenheitToKelvin(fahrenheit):
+
+	kelvin = ((fahrenheit - 32) * (5/9) + 273.15)
+	print("This is the temperature in degrees Kelvin: ", kelvin)
+
+	return round(kelvin, 2)
+
+
+def convertKelvinToCelsius(kelvin):
+	celsius = kelvin - 273.15
+	print("This is the temperature in degrees Celsius: ", celsius)
+
+	return round(celsius, 2)
+
+
+
+def convertKelvinToFahrenheit(kelvin):
+
+	fahrenheit = kelvin * 9/5 - 459.67
+	print("This is the temperature in degrees Fahrenheit: ", fahrenheit)
+
+	return round(fahrenheit, 2)
+
+
+
